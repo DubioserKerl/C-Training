a3c3201bafba8e7a7d87d125ded38f05aed12219
DubioserKerl <daniellosch@mailbox.org>
2018-05-26 10:14:21 +0200

    Command Line Arguments

--- WpfApp1/WpfApp1/App.xaml.cs
+++ WpfApp1/WpfApp1/App.xaml.cs
@@ -8,16 +8,24 @@ using System.Windows;
 
 namespace WpfApp1
 {
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private string[] startupArgs;
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            startupArgs = e.Args;
             // Create the startup window
             MainWindow wnd = new MainWindow();
-            // Show the window
+            if (e.Args.Length >= 2)
+            {
+                wnd.Width = Convert.ToDouble(e.Args[0]);
+                wnd.Height = Convert.ToDouble(e.Args[1]);
+            }
+                // Show the window
             wnd.Show();
         }
     }
